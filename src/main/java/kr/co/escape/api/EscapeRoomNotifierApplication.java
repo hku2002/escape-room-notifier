65bb1d6a8366342d1ea6ffe7e16c062b15a6e593
hku2002 <hku2002@hanmail.net>
2025-11-25 21:24:56 +0900

    first commit

---
+++ src/main/java/kr/co/escape/api/EscapeRoomNotifierApplication.java
@@ -0,0 +1,13 @@
+package kr.co.escape.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscapeRoomNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EscapeRoomNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/escape/api/EscapeRoomNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.escape.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
